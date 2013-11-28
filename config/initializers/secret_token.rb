20f8ed7816a1774a49adf58257da30dab79713a9
EdisonAngela <99637859@qq.com>
2013-11-28 18:48:18 +0800

    first commit
    初始化项目
    
    Signed-off-by: EdisonAngela <99637859@qq.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootstrap::Application.config.secret_token = '1c2fbc2b5d1e52db0d17b21cbad2483f0741997db78c5a7058a7c34fe1b70a59143894bc96a25fe43f20848d3b825f8d50233fa0b5056978fa2c0f1dd41a2cc9'
